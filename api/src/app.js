10fa274eac69d1bf291c617e103c28399e7163b1
Kostya Petukhov <kosyak971@gmailcom>
2021-12-08 17:35:13 +0200

    customization api

---
+++ api/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(
+		`Example app listening at http://${process.env.HOST}:${process.env.PORT}`
+	);
+});
