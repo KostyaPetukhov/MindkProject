757d722b1261b3f9a98d4ed5a0f3bb9669a8d22f
Kostya Petukhov <kosyak971@gmailcom>
2021-12-27 12:23:26 +0200

    Add pre-commit hooks

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,25 @@
 import PostContainer from './containers/Post';
+import React from 'react';
 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <PostContainer
-                    firstName='Ivan'
-                    lastName='Petrov'
-                    date='12.02.2021'
-                    time='17:43'
-                    content='Nice weather today! Its really good day!'
-                />
-            </header>
-        </div>
-    );
+	let a = 3;
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<PostContainer
+					firstName='Ivan'
+					lastName='Petrov'
+					date='12.02.2021'
+					time='17:43'
+					content='Nice weather today! Its really good day!'
+				/>
+			</header>
+		</div>
+	);
 }
 
 export default App;
